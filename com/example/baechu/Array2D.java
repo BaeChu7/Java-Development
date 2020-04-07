5f00cfb2b77482c24532a532274a360e0c482b80
BaeChu7 <choiheeeun77@gmail.com>
2020-04-07 23:36:41 +0900

    2차원 배열 요소 총합 계산

--- com/example/baechu/Array2D.java
+++ com/example/baechu/Array2D.java
@@ -8,30 +8,47 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Array2D {
+//1 1 1 0 0 0
+//0 1 0 0 0 0
+//1 1 1 0 0 0
+//0 9 2 -4 -4 0
+//0 0 0 -2 0 0
+//0 0 -1 -2 -4 0
 /*
-작성자 : 최희은
-작성일 : 2020-04-05
-내 용 : 6 X 6 2차원 배열에서 모래시계 모양 기준으로 각 요소의 합이 가장 큰 값 구하기
-특이사항 :
-*/
+ * 작성자 : 최희은 작성일 : 2020-04-05 내 용 : 6 X 6 2차원 배열에서 모래시계 모양 기준으로 각 요소의 합이 가장 큰 값
+ * 구하기 특이사항 : 2020-04-07) 검증 및 indexoutofbounds 오류 처리
+ */
+public class Array2D {
 	public static int findMaxValue(int[][] arr) {
-		int sum = 0;
 		ArrayList<Integer> sumarr = new ArrayList<Integer>();
+		int[][] valarr;
 		
-		for (int cnt = 0 ; cnt < 4 ; cnt++) {
-			for (int i = cnt ; i < cnt + 3 ; i++ ) {
-				for (int j = cnt ; j < cnt + 3 ; j++ ) {
-					sum	+= arr[i][j];
-					sumarr.add(sum);
-				}
-			}
-		}
 		
+		
+		
+		
+		
+
 		Collections.sort(sumarr, Collections.reverseOrder());
-		
+
 		return sumarr.get(0);
 	}
+
+	/*
+	 * 3 x 3 2차원 배열의 각 요소 합
+	 */
+	public static int sum(int[][] arr) {
+		int sum = 0;
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				sum += arr[i][j];
+			}
+		}
+
+		return sum;
+	}
+
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
