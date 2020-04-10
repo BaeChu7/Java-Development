f96ba0257a48c55c31cd1d0a556c731908150d50
BaeChu7 <choiheeeun77@gmail.com>
2020-04-10 23:17:12 +0900

    3 X3 경우의 수

--- com/example/baechu/Array2D.java
+++ com/example/baechu/Array2D.java
@@ -15,38 +15,32 @@ import java.util.regex.*;
 //0 0 0 -2 0 0
 //0 0 -1 -2 -4 0
 /*
- * 작성자 : 최희은 작성일 : 2020-04-05 내 용 : 6 X 6 2차원 배열에서 모래시계 모양 기준으로 각 요소의 합이 가장 큰 값
- * 구하기 특이사항 : 2020-04-07) 검증 및 indexoutofbounds 오류 처리
+ * 작성자 : 최희은
+ * 작성일 : 2020-04-05
+ * 내 용 : 6 X 6 2차원 배열에서 모래시계 모양 기준으로 각 요소의 합이 가장 큰 값 구하기
+ * 특이사항 :
+ *  2020-04-07) 검증 및 indexoutofbounds 오류 처리
+ *  2020-04-10) 가능한 3 X 3 배열의 합 구하기
  */
 public class Array2D {
 	public static int findMaxValue(int[][] arr) {
 		ArrayList<Integer> sumarr = new ArrayList<Integer>();
-		int[][] valarr;
 		
+		for (int i = 0 ; i < 4 ; i++) {
+			for (int j = 0 ; j < 4 ; j++) {
+				int sum = 0;
+				
+				sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+						+ arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 1][j + 2]
+					    + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+				
+				sumarr.add(sum);
+			}
+		}
 		
-		
-		
-		
-		
-
 		Collections.sort(sumarr, Collections.reverseOrder());
 
 		return sumarr.get(0);
-	}
-
-	/*
-	 * 3 x 3 2차원 배열의 각 요소 합
-	 */
-	public static int sum(int[][] arr) {
-		int sum = 0;
-
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				sum += arr[i][j];
-			}
-		}
-
-		return sum;
 	}
 
 	private static final Scanner scanner = new Scanner(System.in);
