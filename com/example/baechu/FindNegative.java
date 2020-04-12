be6ff6b8baaca40f6019cbbbf256b7916f539312
BaeChu7 <choiheeeun77@gmail.com>
2020-04-12 23:56:14 +0900

    부분 배열

---
+++ com/example/baechu/FindNegative.java
@@ -0,0 +1,22 @@
+package com.example.baechu;
+
+import java.util.Scanner;
+
+/*
+ * 작성자 : 최희은
+ * 작성일 : 2020-04-12
+ * 내 용 : SubArray에서 각 수를 sum한 것 중 음의 값을 갖는 subarray의 갯수
+ */
+public class FindNegative {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        
+        int[] arr = new int[a];
+        for (int i = 0 ; i < a ; i++) {
+        	arr[i] = scan.nextInt();
+        }
+        
+        
+    }
+}
