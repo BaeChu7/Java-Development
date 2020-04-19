870eeab38939a58a4fa13988540ea871fcc5e9b4
BaeChu7 <choiheeeun77@gmail.com>
2020-04-19 20:08:44 +0900

    Iterator

--- com/example/baechu/Comparing.java
+++ com/example/baechu/Comparing.java
@@ -1,7 +1,11 @@
 package com.example.baechu;
 
 import java.util.*;
-
+/*
+ * 작성자 : 최희은
+ * 작성일 : 2020-04-19
+ * 내 용 : 두 개의 기준대로 정렬하기
+ */
 class Student1{
 	private int id;
 	private String fname;
@@ -25,25 +29,27 @@ class Student1{
 }
 
 //Complete the code
-public class TwoSort
+public class Comparing
 {
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
-		
+
 		List<Student1> studentList = new ArrayList<Student1>();
-		while(testCases > 0){
+		while (testCases > 0) {
 			int id = in.nextInt();
 			String fname = in.next();
 			double cgpa = in.nextDouble();
-			
+
 			Student1 st = new Student1(id, fname, cgpa);
 			studentList.add(st);
-			
+
 			testCases--;
 		}
-      
-      	for(Student1 st: studentList){
+
+		Collections.sort(studentList,  Comparator.comparing(Student1 :: getCgpa).reversed().thenComparing(Student1 :: getFname).thenComparing(Student1 :: getId));
+
+		for (Student1 st : studentList) {
 			System.out.println(st.getFname());
 		}
 	}
--- com/example/baechu/InstanceOf.java
+++ com/example/baechu/InstanceOf.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 /*
 작성자 : 최희은
-작성일 : 
-내   용 : Test
+작성일 : 2019-04-19
+내   용 : ArrayList 안에 있는 요소들 중 String만 출력하기
 */
 import java.util.*;
 
-public class Solution {
+public class InstanceOf {
 
 	static Iterator func(ArrayList mylist) {
 		Iterator it = mylist.iterator();
+		
 		while (it.hasNext()) {
 			Object element = it.next();
 
-			if (element instanceof String) // Hints: use instanceof operator
+			if (element instanceof String) // element가 String 형이면 true 그렇지 않으면 false 반환
 
 				break;
 		}
@@ -24,7 +25,6 @@ public class Solution {
 		return it;
 	}
 
-	@SuppressWarnings({ "unchecked" })
 	public static void main(String[] args) {
 		ArrayList mylist = new ArrayList();
 		Scanner sc = new Scanner(System.in);
@@ -37,11 +37,13 @@ public class Solution {
 		}
 
 		mylist.add("###");
+		
 		for (int i = 0; i < m; i++) {
 			mylist.add(sc.next());
 		}
 
-		Iterator it = func(mylist);
+		Iterator it = func(mylist); // func에서 숫자까지 이미 체크하고 break문으로 빠져나와 연결해서 체크하는 것이기 때문에 그 다음 항목부터 체크
+		
 		while (it.hasNext()) {
 			Object element = it.next();
 			System.out.println((String) element);
