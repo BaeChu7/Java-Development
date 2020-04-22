6845990ab4187091d4679256e6597e767fba6ddb
BaeChu7 <choiheeeun77@gmail.com>
2020-04-22 18:17:42 +0900

    솔류션 테스트

--- com/example/baechu/Solution.java
+++ com/example/baechu/Solution.java
@@ -1,16 +1,14 @@
 package com.example.baechu;
+
+import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
 
 
-public class Solution{
+class Solution{
 	
-	public static void main(String []args){
-		Scanner sc=new Scanner(System.in);
-		String title=sc.nextLine();
-		MyBook1 new_novel=new MyBook1();
-		new_novel.setTitle(title);
-		System.out.println("The title is: "+new_novel.getTitle());
-      	sc.close();
-		
-	}
+	public static void main(String[] args) {}
 }
